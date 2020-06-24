0bad0dd5ba17127427fce164fb0368f74c5e9e20
truonghh99 <truonghh@beloit.edu>
2020-06-24 11:25:25 -0700

    Implement RecyclerView

--- app/src/main/java/com/example/flixster/models/Movie.java
+++ app/src/main/java/com/example/flixster/models/Movie.java
@@ -28,7 +28,7 @@ public class Movie {
     }
 
     public String getPosterPath() {
-        return String.format("https://image.tmdb.org/t/p/w342/$s", posterPath);
+        return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
     }
 
     public String getTitle() {
