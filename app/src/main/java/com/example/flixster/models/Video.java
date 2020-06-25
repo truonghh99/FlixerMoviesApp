ac1a2a189abcb075611d94dd42505f1adc8473bf
truonghh99 <truonghh@beloit.edu>
2020-06-25 09:36:50 -0700

    Stablize YoutubePlayer

---
+++ app/src/main/java/com/example/flixster/models/Video.java
@@ -0,0 +1,28 @@
+package com.example.flixster.models;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Video {
+    String videoKey;
+
+    public Video(JSONObject jsonObject) throws JSONException {
+        videoKey = jsonObject.getString("key");
+    }
+
+    public static List<Video> extractVideosFromJsonArray(JSONArray videoJsonArray) throws JSONException {
+        List<Video> videos = new ArrayList<>();
+        for (int i = 0; i < videoJsonArray.length(); ++i) {
+            videos.add(new Video(videoJsonArray.getJSONObject(i)));
+        }
+        return videos;
+    }
+
+    public String getVideoKey() {
+        return videoKey;
+    }
+}
