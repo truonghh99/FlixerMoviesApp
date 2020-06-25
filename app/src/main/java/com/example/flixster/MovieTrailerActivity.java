893ec6217812e80d37ca75bcc92bfe2757f45a1f
truonghh99 <truonghh@beloit.edu>
2020-06-25 09:36:50 -0700

    Stablize YoutubePlayer

--- app/src/main/java/com/example/flixster/MovieTrailerActivity.java
+++ app/src/main/java/com/example/flixster/MovieTrailerActivity.java
@@ -10,6 +10,7 @@ import android.view.WindowManager;
 import com.codepath.asynchttpclient.AsyncHttpClient;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 import com.example.flixster.models.Movie;
+import com.example.flixster.models.Video;
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
@@ -19,35 +20,56 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import okhttp3.Headers;
 
 public class MovieTrailerActivity extends YouTubeBaseActivity {
 
-    public final String VIDEO_REQUEST_URL_1 = "https://api.themoviedb.org/3/movie/";
-    public final String VIDEO_REQUEST_URL_2 = "/videos?api_key=cd0afcef53a10158136ff1781c5e5e0d&language=en-US";
-    public String videoKey;
+    private final String VIDEO_REQUEST_URL_1 = "https://api.themoviedb.org/3/movie/";
+    private final String VIDEO_REQUEST_URL_2 = "/videos?api_key=cd0afcef53a10158136ff1781c5e5e0d&language=en-US";
     private final String API_KEY = "AIzaSyAyFJBF17-aFYTBo5FjmiRH6LRPcBWM7wc";
+    private final String TAG = "MovieTrailerActivity";
+    private List<Video> videos;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+/*        requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);*/
         setContentView(R.layout.activity_movie_trailer);
 
-        String movieId = getIntent().getExtras().getString(InfoActivity.KEY_ID);
-        getVideoId(VIDEO_REQUEST_URL_1 + movieId + VIDEO_REQUEST_URL_2);
+        String videoUrl = getUrl();
+        getVideos(videoUrl);
+        Log.e(TAG, "done");
 
-        // resolve the player view from the layout
+        try {
+            TimeUnit.MILLISECONDS.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         YouTubePlayerView playerView = (YouTubePlayerView) findViewById(R.id.player);
-
         // initialize with API key stored in secrets.xml
         playerView.initialize(API_KEY, new YouTubePlayer.OnInitializedListener() {
             @Override
             public void onInitializationSuccess(YouTubePlayer.Provider provider,
                                                 YouTubePlayer youTubePlayer, boolean b) {
-                Log.e("Heyyy", videoKey);
-                youTubePlayer.cueVideo(videoKey);
+                // Loop through all the given videos until found a valid one
+                for (int i = 0; i < videos.size(); ++i) {
+                    try {
+                        String videoKey = videos.get(i).getVideoKey();
+                        Log.e(TAG, videoKey);
+                        youTubePlayer.setFullscreen(true);
+                        youTubePlayer.loadVideo(videoKey);
+                        break;
+                    } catch (Exception e) {
+                        Log.e(TAG, "Failed to load video " + i + "/nError: " + e);
+                    }
+                }
             }
 
             @Override
@@ -58,25 +80,33 @@ public class MovieTrailerActivity extends YouTubeBaseActivity {
         });
     }
 
-    private void getVideoId(String URL) {
+    private String getUrl() {
+        String movieId = getIntent().getExtras().getString(InfoActivity.KEY_ID);
+        String videoUrl = VIDEO_REQUEST_URL_1 + movieId + VIDEO_REQUEST_URL_2;
+        Log.e(TAG, videoUrl);
+        return videoUrl;
+    }
+
+    private synchronized void getVideos(String videoUrl) {
         AsyncHttpClient client = new AsyncHttpClient();
-        // Request movies' information from now playing url
-        client.get(URL, new JsonHttpResponseHandler() {
+        videos = new ArrayList<Video>();
+
+        client.get(videoUrl, new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Headers headers, JSON json) {
                 JSONObject jsonObject = json.jsonObject;
                 try {
                     JSONArray results = jsonObject.getJSONArray("results");
-                    JSONObject video = (JSONObject) results.get(0);
-                    videoKey = video.getString("key");
+                    videos = Video.extractVideosFromJsonArray(results);
+                    Log.e(TAG, "Size " + String.valueOf(videos.size()));
                 } catch (JSONException e) {
-                    Log.e("TrailerActivity", "Failed to extract video key");
+                    Log.e(TAG, "Failed to extract video key");
                 }
             }
 
             @Override
             public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
-                Log.d("TrailerActivity", "onFailure");
+                Log.d("TAG", "onFailure");
             }
         });
     }
