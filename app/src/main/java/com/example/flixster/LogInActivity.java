1088a67eda9710183914963eca16e4d84f2b5ea5
truonghh99 <truonghh@beloit.edu>
2020-06-25 21:50:38 -0700

    Add login activity & establish Firebase connection

---
+++ app/src/main/java/com/example/flixster/LogInActivity.java
@@ -0,0 +1,71 @@
+package com.example.flixster;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.flixster.databinding.ActivityInfoBinding;
+import com.example.flixster.databinding.ActivityLogInBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LogInActivity extends AppCompatActivity {
+
+    private EditText etEmail;
+    private EditText etPassword;
+    private Button btnLogin;
+    private TextView tvRegister;
+    private String email;
+    private String password;
+    private FirebaseAuth fAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityLogInBinding binding = ActivityLogInBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        etEmail = binding.etEmailLogin;
+        etPassword = binding.etPassword;
+        btnLogin = binding.btnLogin;
+        tvRegister = binding.tvRegister;
+
+        email = etEmail.getText().toString();
+        password = etPassword.getText().toString();
+        fAuth = FirebaseAuth.getInstance();
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(), "Logged in successfully!", Toast.LENGTH_SHORT);
+                            startActivity(new Intent(LogInActivity.this, MainActivity.class));
+                        } else {
+                            Toast.makeText(getApplicationContext(), task.getException().toString(), Toast.LENGTH_SHORT);
+                        }
+                    }
+                });
+            }
+        });
+
+        tvRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+    }
+}
